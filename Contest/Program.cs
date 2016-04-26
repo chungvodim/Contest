5cab752bb358f2867205096276b9b1ee8169c659
chungvodim <ba198647@gmail.com>
2016-04-26 20:09:14 +0800

    update Hangman

--- Contest/Program.cs
+++ Contest/Program.cs
@@ -164,66 +164,105 @@ namespace Contest
         public static List<string> LoadCombos()
         {
             List<string> Combos = new List<string>();
-            Combos.Add("L-LD-D-RD-R-P");
+            //Combos.Add("L-LD-D-RD-R-P");
+            //Combos.Add("R-RD-D-LD-L-K");
             Combos.Add("D-RD-R-P");
             Combos.Add("R-D-RD-P");
             Combos.Add("D-LD-L-K");
-            Combos.Add("R-RD-D-LD-L-K");
             return Combos;
         }
 
         public static int GetNumberOfFailedCombos(List<string> Combos, string session)
         {
             int result = 0;
-            string[] steps = session.Split('-');
+            List<string> MissedCombos = new List<string>();
             foreach (var combo in Combos)
             {
+                int missedComboCount = 0;
+                int matchedComboCount = 0;
                 var missCombo = combo.Substring(0, combo.Length - 2);
-                result += Regex.Matches(session, missCombo).Count;
-                result -= Regex.Matches(session, combo).Count;
+
+                missedComboCount = Regex.Matches(session, missCombo).Count;
+                matchedComboCount = Regex.Matches(session, combo).Count;
+
+                result += (missedComboCount - matchedComboCount);
             }
-            //while (steps.Length > 0 && !isEnd)
-            //{
-            //    for (int i = 0; i < steps.Length; i++)
-            //    {
-            //        step = steps[i];
-            //        var temp = Combos;
-            //        Combos = Combos.Where(x => x[i] == step).ToList();
-            //        if (Combos.Count == 0 && temp.Any(x => (x.Length - i) == 1))
-            //        {
-            //            result++;
-            //            steps = steps.Skip(i + 1).ToArray();
-            //            break;
-            //        }
-            //        if (i == steps.Length - 1)
-            //        {
-            //            if(temp.Any(x => (x.Length - i) == 2))
-            //            {
-            //                result++;
-            //            }
-            //            isEnd = true;
-            //        }
-            //    }
-            //}
             return result;
         }
         static void Main(string[] args)
         {
             try
             {
-                string[] lines = File.ReadAllLines("sampleInput1.txt");
-                List<string> combos = LoadCombos();
-                int t = Convert.ToInt32(lines[0]);
-                for (int i = 1; i <= t; i++)
+                string[] words = File.ReadAllLines("words.txt");
+                Random random = new Random((int)DateTime.Now.Ticks);
+                string wordToGuess = words[random.Next(0, words.Length)];
+                Console.WriteLine("word to guess: {0}", wordToGuess);
+                string wordToGuessUppercase = wordToGuess.ToUpper();
+                StringBuilder displayToPlayer = new StringBuilder(wordToGuess.Length);
+                for (int i = 0; i < wordToGuess.Length; i++)
+                    displayToPlayer.Append('_');
+
+                List<char> correctGuesses = new List<char>();
+                List<char> incorrectGuesses = new List<char>();
+                int lives = 5;
+                bool won = false;
+                int lettersRevealed = 0;
+
+                string input;
+                char guess;
+
+                while (!won && lives > 0)
                 {
-                    string sessions = lines[i];
-                    var result = GetNumberOfFailedCombos(combos, sessions);
-                    Console.WriteLine(String.Format("Case #{0}: {1}", i, result));
+                    Console.Write("Guess a letter: ");
+
+                    input = Console.ReadLine().ToUpper();
+                    guess = input[0];
+
+                    if (correctGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
+                        continue;
+                    }
+                    else if (incorrectGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
+                        continue;
+                    }
+
+                    if (wordToGuessUppercase.Contains(guess))
+                    {
+                        correctGuesses.Add(guess);
+
+                        for (int i = 0; i < wordToGuess.Length; i++)
+                        {
+                            if (wordToGuessUppercase[i] == guess)
+                            {
+                                displayToPlayer[i] = wordToGuess[i];
+                                lettersRevealed++;
+                            }
+                        }
+
+                        if (lettersRevealed == wordToGuess.Length)
+                            won = true;
+                    }
+                    else
+                    {
+                        incorrectGuesses.Add(guess);
+
+                        Console.WriteLine("Nope, there's no '{0}' in it!", guess);
+                        lives--;
+                    }
+
+                    Console.WriteLine(displayToPlayer.ToString());
                 }
-                //List<State> lstState = ParseCode(lines);
-                //List<Tape> lstTape = GetTapes(lines);
-                //Interpreter(lstState, lstTape);
-                //Console.WriteLine("done");
+
+                if (won)
+                    Console.WriteLine("You won!");
+                else
+                    Console.WriteLine("You lost! It was '{0}'", wordToGuess);
+
+                Console.Write("Press ENTER to exit...");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
